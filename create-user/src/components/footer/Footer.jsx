dfdcf4011d3457cc4165048161e4e01e65720452
Asrorbek477 <asrorbeksaidmurodov2004@gmail.com>
2024-03-31 12:35:18 +0500

    new proyekt

---
+++ create-user/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+      </div>
+    </div>
+  )
+}
+
+export default Footer
